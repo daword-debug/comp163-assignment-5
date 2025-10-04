dd7b707818b2cabae6ac98946a357d11170faeee
daword-debug <daword@aggies.ncat.edu>
2025-10-03 22:03:25 -0400

    Create darin_assignment5V2.py
    Part 2

---
+++ darin_assignment5V2.py
@@ -0,0 +1,30 @@
+print("=== Challenge 1: Collatz Conjecture ===")
+n = int(input("Enter starting number:"))
+step_count = 0 
+print(" Sequence:",n, end=" ")
+while n != 1:
+    if n % 2 == 0:
+        n = n // 2
+    else:
+        n = (n * 3) + 1
+    step_count += 1
+
+    print(n,end=" ")
+
+print()
+print("Steps:",step_count)
+print()
+print("=== Challenge 2: Prime Number Checker ===")
+current_number = int(input(f"Enter a number: "))
+print(f"Testing divisors from 2 to {current_number - 1}")
+step_count = 0
+
+for i in range (2, (current_number-1)):
+    if current_number % i == 0:
+        current_number / 2
+        print(f"{current_number} is not prime!")
+        break
+else:
+    print(f"{current_number} is prime!")
+
+print()
